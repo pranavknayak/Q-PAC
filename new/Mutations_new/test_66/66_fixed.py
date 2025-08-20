6e1eb3c1791be2c0650c0f4d74ed6e1044d53609
Ahmik Virani <ahmikvirani44@gmail.com>
2025-08-20 11:57:03 +0530

    Added the incorrect number of samples testcases

---
+++ new/Mutations_new/test_66/66_fixed.py
@@ -0,0 +1,7 @@
+def eigenvalue_estimate(n_shots):
+    pass
+
+eig3 = eigenvalue_estimate(5)
+eig4 = eigenvalue_estimate(6)
+eig5 = eigenvalue_estimate(4)
+lambda_min = min([eig1, eig2, eig3, eig4, eig5])
---
+++ new/Mutations_new/test_68/68_buggy.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def fidelity(estimate, reference):
+    # Compute fidelity with respect to some baseline
+    pass
+
+fid1 = fidelity(est1, ref)
+fid2 = fidelity(est2, ref)
+avg_fid = np.mean([fid1, fid2])
