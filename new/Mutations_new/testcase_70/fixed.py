e296ef136e8b6c0861f3a1d1634d884633556d8f
Ahmik Virani <ahmikvirani44@gmail.com>
2025-08-27 19:00:07 +0530

    Ensuring that exec runs in the dataset by adding import

--- new/Mutations_new/testcase_70/fixed.py
+++ new/Mutations_new/testcase_70/fixed.py
@@ -1,5 +1,5 @@
 from qiskit import QuantumCircuit
-qc = QuantumCircuit(2)
+qc = QuantumCircuit(2, 2)
 qc.x(0)
 qc.h(1)
 qc.measure(0, 0)
--- new/Mutations_new/testcase_74/buggy.py
+++ new/Mutations_new/testcase_74/buggy.py
@@ -3,5 +3,5 @@ from qiskit.circuit import Gate
 qc = QuantumCircuit(4, 4)
 gt1 = Gate('gt', 3, []) 
 gt2 = Gate('gt2', 3, [])
-qc.append(gt, [0, 1, 2])
+qc.append(gt1, [0, 1, 2])
 qc.append(gt2, [1, 2, 3])
