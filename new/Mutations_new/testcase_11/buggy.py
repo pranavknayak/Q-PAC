267a5db1208af786dbb1d74e06a4c91774f321d3
Ahmik Virani <ahmikvirani44@gmail.com>
2025-08-27 20:43:14 +0530

    Updated dataset

--- new/Mutations_new/testcase_11/buggy.py
+++ new/Mutations_new/testcase_11/buggy.py
@@ -4,10 +4,10 @@ q = QuantumRegister(2)
 c = ClassicalRegister(2) 
 grover_circuit = QuantumCircuit(q, c)
 # grover_circuit = initialize_s(grover_circuit, [0, 1])
-grover_circuit.cz(q[0], q[1])
+grover_circuit.cz(0, 1)
 grover_circuit.measure_all()  # First measurement before the diffusion operator
 grover_circuit.h([0, 1])  # Diffusion operator
 grover_circuit.z([0, 1])
-grover_circuit.cz(q[0], q[1])
+grover_circuit.cz(0, 1)
 grover_circuit.h([0, 1])
 grover_circuit.measure_all()  # Second measurement after diffusion operator"
