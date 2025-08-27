3add0fc1b479ea7556a40858dd9899759beb5a05
Ahmik Virani <ahmikvirani44@gmail.com>
2025-08-27 19:25:31 +0530

    Updated the testcase 21

--- new/Mutations_new/testcase_21/buggy.py
+++ new/Mutations_new/testcase_21/buggy.py
@@ -5,7 +5,7 @@ c = ClassicalRegister(2)
 grover_circuit = QuantumCircuit(q, c)
 # grover_circuit = initialize_s(grover_circuit, [0, 1])
 grover_circuit.cz(0,1)  # Oracle
-grover_circuit.i([0,1])  # Incorrect: Replacing H gate with Identity gate in diffusion
+grover_circuit.id([0,1])  # Incorrect: Replacing H gate with Identity gate in diffusion
 grover_circuit.z([0,1])
 grover_circuit.cz(0,1)
-grover_circuit.i([0,1])  # Incorrect: Replacing H gate with Identity gate"
+grover_circuit.id([0,1])  # Incorrect: Replacing H gate with Identity gate"
